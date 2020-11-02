e6aa6d462bc289af60c3d1bc5b5588f15c121ec6
Fabio Godoy <fabio.godoy@oldpocket.com>
2020-11-02 13:55:31 -0300

    interfaces

--- routes/routes_authorization.php
+++ routes/routes_authorization.php
@@ -1,9 +1,33 @@
 <?php
 
-
 /**
  * HTTP Auth - Minimalist authentication to return a JWT
  */
 $router
-    ->on('GET', '/authorization/roles/device/', function () use ($router) {
+
+    /**
+    * POST authorizationRolesRoleUIDPost
+    * Summary: Create a new role in the system
+    * Notes: 
+    * Output-Formats: [application/json]
+    */
+    ->on('POST', '/authorization/roles/(\w+)', function ($roleUID) use ($router) {
+    })
+    
+    /**
+    * POST authorizationRoutesRouteUIDPost
+    * Summary: Register a new route in the system
+    * Notes: 
+    * Output-Formats: [application/json]
+    */
+    ->on('POST', '/authorization/routes/(\w+)', function ($roteUID) use ($router) {
+    })
+    
+    /**
+    * POST authorizationPermissionRoleUIDRouteUIDPost
+    * Summary: Associate a route with a role
+    * Notes: 
+    * Output-Formats: [application/json]
+    */
+    ->on('POST', '/authorization/permission/(\w+)/(\w+)', function ($roleUID, $routeUID) use ($router) {
     });
--- routes/routes_users.php
+++ routes/routes_users.php
@@ -5,5 +5,39 @@
  * HTTP Auth - Minimalist authentication to return a JWT
  */
 $router
+
+    /**
+    * Get usersGet
+    * Summary: Create a new user in the system
+    * Notes: 
+    * Output-Formats: [application/json]
+    */
     ->on('GET', '/users', function () use ($router) {
+    })
+
+    /**
+    * Get usersUsernameGet
+    * Summary: Create a new user in the system
+    * Notes: 
+    * Output-Formats: [application/json]
+    */
+    ->on('GET', '/users/(\w+)', function ($user) use ($router) {
+    })
+    
+    /**
+    * POST usersUsernamePost
+    * Summary: Create a new user in the system
+    * Notes: 
+    * Output-Formats: [application/json]
+    */
+    ->on('POST', '/users/(\w+)', function ($user) use ($router) {
+    })
+
+    /**
+    * PUT usersUsernamePasswordPut
+    * Summary: Create a new user in the system
+    * Notes: 
+    * Output-Formats: [application/json]
+    */
+    ->on('PUT', '/users/(\w+)/password', function ($user) use ($router) {
     });
