a5f457332a9b88bd2fe96378790fd1733b9ea0c6
Fabio Godoy <fabio.godoy@oldpocket.com>
2020-11-08 21:29:55 -0300

    Implementation of authorization routes

--- routes/routes_authorization.php
+++ routes/routes_authorization.php
@@ -12,6 +12,29 @@ $router
     * Output-Formats: [application/json]
     */
     ->on('POST', '/authorization/roles/(\w+)', function ($roleUID) use ($router) {
+        $qb = new QueryBuilder();
+        $r = $qb
+            ->table('roles')
+            ->fields(['id'])
+            ->where(["uid = '$roleUID'"])
+            ->select();
+        if (count($r->values) > 0) 
+            throw new HttpException(409, "Duplicated role UID: $roleUID");
+        
+        $data = $router->body;
+        $role_id = $qb
+            ->table('roles')
+            ->fields(['uid', 'description'])
+            ->insert([$roleUID, $data->description]);
+        
+        $role = $qb
+            ->table('roles')
+            ->fields(['uid', 'description'])
+            ->where(["id = '$role_id'"])
+            ->selectOne();
+
+        return $role;
+
     })
     
     /**
@@ -20,7 +43,29 @@ $router
     * Notes: 
     * Output-Formats: [application/json]
     */
-    ->on('POST', '/authorization/routes/(\w+)', function ($roteUID) use ($router) {
+    ->on('POST', '/authorization/routes/(\w+)', function ($routeUID) use ($router) {
+        $qb = new QueryBuilder();
+        $r = $qb
+            ->table('routes')
+            ->fields(['id'])
+            ->where(["uid = '$routeUID'"])
+            ->select();
+        if (count($r->values) > 0) 
+            throw new HttpException(409, "Duplicated route UID: $routeUID");
+        
+        $data = $router->body;
+        $route_id = $qb
+            ->table('routes')
+            ->fields(['uid', 'description', 'route', 'verb'])
+            ->insert([$routeUID, $data->description, $data->route, $data->verb]);
+        
+        $route = $qb
+            ->table('routes')
+            ->fields(['uid', 'description', 'route', 'verb'])
+            ->where(["id = '$route_id'"])
+            ->selectOne();
+
+        return $route;
     })
     
     /**
@@ -30,4 +75,29 @@ $router
     * Output-Formats: [application/json]
     */
     ->on('POST', '/authorization/permission/(\w+)/(\w+)', function ($roleUID, $routeUID) use ($router) {
+        $qb = new QueryBuilder();
+        $route = $qb
+            ->table('routes')
+            ->fields(['id'])
+            ->where(["uid = '$routeUID'"])
+            ->select();
+        if (count($route->values) == 0) 
+            throw new HttpException(404, "Route UID not found: $routeUID");
+        $route_id = $route->values[0]->id;
+
+        $role = $qb
+            ->table('roles')
+            ->fields(['id'])
+            ->where(["uid = '$roleUID'"])
+            ->select();
+        if (count($role->values) == 0) 
+            throw new HttpException(404, "Role UID not found: $roleUID");
+        $role_id = $role->values[0]->id;
+
+        $roles_routes_id = $qb
+            ->table('roles_routes')
+            ->fields(['routes_id', 'roles_id'])
+            ->insert([$route_id, $role_id]);
+
+        return array('OK');
     });
--- routes/routes_devices.php
+++ routes/routes_devices.php
@@ -6,68 +6,6 @@
  */
 $router
     /**
-     * HTTP Auth - Minimalist authentication to return a JWT
-     */
-    ->on('GET', '/auth', function () {
-        
-        $auth_user = $_SERVER['PHP_AUTH_USER'];
-        $auth_passwd = $_SERVER['PHP_AUTH_PW'];
-        
-        if (!$auth_user) 
-            throw new HttpException(400, "No user informed");
-        
-        $qb = new QueryBuilder();
-        
-        // Looking for the user token
-        $tokens = $qb
-            ->table('tokens')
-            ->fields(['id', 'roles_id', 'secret', 'password', 'username'])
-            ->where(["username = '$auth_user'"])
-            ->select();
-        // Each user must have just one token
-        if (count($tokens->values) != 1) 
-            throw new HttpException(400, "Duplicated username in the system");
-
-        $token = $tokens->values[0];
-        if ($token == NULL || !password_verify($auth_passwd, $token->password))
-            throw new HttpException(401, 'Username or password does not match');
-
-        // Getting the allowed routes for this user
-        $roles_id = $token->roles_id;
-        $roles_routes = $qb
-            ->table('roles_routes')
-            ->fields(['routes_id'])
-            ->where(["roles_id = $roles_id"])
-            ->select();
-
-        $allowed_routes = array();
-        foreach ($roles_routes->values as $value) {
-            array_push($allowed_routes, $value->routes_id);
-        }
-
-        // All user should have at least one route, otherwise he/she/it doesnt 
-        // have what to do in the system
-        if (count($allowed_routes) == 0) 
-            throw new HttpException(400, "No routes allowed for this user");
-        
-        // Preparing user's JWT payload
-        $timestamp = time();
-        $payload = array(
-            'username' => $token->username,  // logged in user 
-            'allowed_routes' => $allowed_routes,
-            'exp' => $timestamp + 6000, // token expiration timeout in seconds
-            'iat' => $timestamp
-        );
-
-        // ($payload, $secret, algorithm)
-        $jwt = JWTHelper::encode($payload, $token->secret, $token->id,'HS256');
-        $result = ['token' => $jwt];
-
-        // Returning the JWT
-        return $result;
-    })
-
-    /**
     * GET deviceGet
     * Summary: Return device details
     * Notes: 
